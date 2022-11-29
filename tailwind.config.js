fcc757d00ff8b38792e3cbd6afa9f55996f22de8
wi1dcode <100387382+wi1dcode@users.noreply.github.com>
2022-11-29 13:55:01 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
   corePlugins: {
     preflight: false,
-  },
+  }
 };
